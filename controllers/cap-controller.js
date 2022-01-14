866a6890e2e4f4cf185ad21e4d9b98b404f59e4c
tbellenger <tbellenger@gmail.com>
2022-01-13 20:43:30 -0800

    add playwright npx install

--- controllers/cap-controller.js
+++ controllers/cap-controller.js
@@ -4,9 +4,13 @@ const capController = {
   async getCapture(req, res) {
     try {
       const uri = req.query.uri;
-      console.log("URL:" + uri);
-      res.contentType("image/jpeg");
-      res.send(await createScreenCapture(uri));
+      if (uri) {
+        console.log("URL:" + uri);
+        res.contentType("image/jpeg");
+        res.send(await createScreenCapture(uri));
+      } else {
+        res.status(400).json({ message: "usage: /api/cap?uri=xxx" });
+      }
     } catch (err) {
       res.status(500).json(err);
     }
